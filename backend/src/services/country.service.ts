01b73aa79f4d70506369402a2a559a09fec31d71
ruslanTen <ruslantendora@gmail.com>
2025-02-18 15:35:18 +0200

    fix: backend base URL

--- backend/src/services/country.service.ts
+++ backend/src/services/country.service.ts
@@ -1,19 +1,16 @@
 import axios, { Axios } from 'axios';
 
-const DATE_NAGER_API = 'https://date.nager.at/api/v3';
-const COUNTRIES_NOW_API = 'https://countriesnow.space/api/v0.1/countries';
-
 export default class CountriesService {
 	private dateNagerApi: Axios;
 	private countriesNowApi: Axios;
 
 	constructor() {
 		this.dateNagerApi = axios.create({
-			baseURL: DATE_NAGER_API,
+			baseURL: process.env.DATE_NAGER_API,
 		});
 
 		this.countriesNowApi = axios.create({
-			baseURL: COUNTRIES_NOW_API,
+			baseURL: process.env.COUNTRIES_NOW_API,
 		});
 	}
 	async fetchAvailableCountries() {
