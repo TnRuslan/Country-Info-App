b87d86d31de157d578d06cdfc1c50fae02205412
ruslanTen <ruslantendora@gmail.com>
2025-02-18 23:43:15 +0200

    fix: query client provider

--- frontend/src/utils/Providers/ReactQueryProvider.tsx
+++ frontend/src/utils/Providers/ReactQueryProvider.tsx
@@ -1,10 +1,9 @@
 'use client';
 
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { useState } from 'react';
 
 const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
-	const [queryClient] = useState(() => new QueryClient());
+	const queryClient = new QueryClient();
 
 	return (
 		<QueryClientProvider client={queryClient}>
